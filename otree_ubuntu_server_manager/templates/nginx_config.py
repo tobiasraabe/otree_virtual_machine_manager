c9edfbf1fae1cf367c22d0c9600a36a0be6061c6
Felix Albrecht <felix.albrecht.priv@gmail.com>
2017-01-17 09:38:10 +0100

    nginx config correction - HTTPPORT now unnecessary

--- otree_ubuntu_server_manager/templates/nginx_config.py
+++ otree_ubuntu_server_manager/templates/nginx_config.py
@@ -13,15 +13,18 @@ server {
     ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
     ssl_ciphers 'EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH';
 
-# create your own ssl certification using e.g. letsencrypt (installed on system)
+# replace the 2 lines below with your own ssl certificates using e.g. letsencrypt (installed on system)
     ssl_certificate /etc/ssl/certs/ssl-cert-snakeoil.pem;
     ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;
 
     add_header Strict_Transport-Secutiry max-age=31536000;
-    #ssl_dhparm /etc/nginx/ssl/dhparm.pem;
+    ssl_dhparam /etc/nginx/ssl/dhparam.pem;
     ssl_session_cache shared:ssl_session_cache:10m;
 
-    root /home/otree-admin/otree_project;
+# reroute http request on this port to https
+    error_page 497 https://$host:SSLPORT$request_uri;
+
+    root OTREEHOME;
     index index.html index.htm index.nginx-debian.html;
 
     server_name _;
@@ -37,16 +40,11 @@ server {
             proxy_http_version 1.1; # protocol switch needed
             proxy_set_header Upgrade $http_upgrade;
             proxy_set_header Connection $connection_upgrade; # End of the configuration for oTree
-            proxy_set_header HOST $host:HTTPPORT;
+# add ssl port to all server addresses
+            proxy_set_header HOST $host:SSLPORT;
             proxy_set_header X-Real-Ip $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Host $server_name;
     }
-}
-
-server {
-    listen HTTPPORT;
-    server_name _;
-    return 301 https://$host:SSLPORT$request_uri;
 }
 """
